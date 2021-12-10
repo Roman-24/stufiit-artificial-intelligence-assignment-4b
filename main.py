287a949485d744a026edd8d6233d170fd12fe935
Roman Bitarovský <bitarovskyroman@gmail.com>
2021-12-10 19:54:17 +0100

    refactor

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import random
 import math
 import copy
-import numpy as np
 import time
 from tkinter import *
 
 NUM_OF_POINTS = 20000
 INTERVAL = 5000
 DEVIATION = 100
-WINDOW_SIZE = 360
+WINDOW_SIZE = 720
 
 colors = ['dodgerblue', 'red', 'gold', 'forestgreen', 'orange', 'midnightblue', 'darkgreen', 'darkkhaki',
           'salmon', 'deeppink', 'dimgrey', 'seagreen', 'cyan', 'saddlebrown', 'springgreen', 'violetred',
@@ -22,8 +21,7 @@ class Point:
         self.cluster_id = None
         self.not_in_cluster = True
 
-
-# generate first 20 points
+# prvych 20 unikatnych points
 def init():
     first_20 = []
     unique_rand_nums = set()
@@ -43,23 +41,23 @@ def init():
 def generate_others(first_20):
     all_points = []
     all_points += first_20
-    counter = 20
+    counter = 0
 
-    while counter < NUM_OF_POINTS + 20:
+    while counter < NUM_OF_POINTS:
         point = random.choice(all_points)
 
         x_offset = int(random.gauss(-DEVIATION, DEVIATION))
         y_offset = int(random.gauss(-DEVIATION, DEVIATION))
+
         new_point = Point(point.x + x_offset, point.y + y_offset)
         all_points.append(new_point)
         counter += 1
 
     return all_points
 
-# calculate distance between 2 points
+# vypocita vzdialenost dvoch bodov
 def euclidean_dist(point_1, point_2):
     return math.sqrt(math.pow(point_1.x - point_2.x, 2) + math.pow(point_1.y - point_2.y, 2))
-
 
 def k_means(k, points, type):
 
@@ -87,21 +85,16 @@ def k_means(k, points, type):
     pass
 
 def coordinates(point):
-    size = (INTERVAL + DEVIATION)
+    size = (INTERVAL + DEVIATION) * 4
     left = WINDOW_SIZE / 2 - 1
     right = WINDOW_SIZE / 2 + 1
-    return int(point.x / size * WINDOW_SIZE + left), int(point.y / size * WINDOW_SIZE + left), \
-           int(point.x / size * WINDOW_SIZE + right), int(point.y / size * WINDOW_SIZE + right)
-
+    return int(point.x / size * WINDOW_SIZE + left), int(point.y / size * WINDOW_SIZE + left), int(point.x / size * WINDOW_SIZE + right), int(point.y / size * WINDOW_SIZE + right)
 
 def draw(points):
     master = Tk()
     master.title("Clusters")
-    canvas = Canvas(master, width=WINDOW_SIZE + 80, height=WINDOW_SIZE + 20, bg='whitesmoke')
+    canvas = Canvas(master, width=WINDOW_SIZE, height=WINDOW_SIZE, bg='whitesmoke')
     canvas.pack()
-    label = Label(master, textvariable=StringVar())
-    label.place(x=WINDOW_SIZE + 75, y=50, anchor=E)
-    counter = 1
 
     for point in points:
         canvas.create_oval(coordinates(point), fill=colors[point.cluster_id], outline='')
